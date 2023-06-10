0e4d2ecc2f1b4e4da86e9b84cca2fa4bb2fd47ea
dulithaprasanna <dulithaprasanna@gmail.com>
2023-06-10 11:29:00 -0500

    Adding EDBOp results strategy

--- src/pyoptimizer_analysis/EDBOpResultsStrategy.py
+++ src/pyoptimizer_analysis/EDBOpResultsStrategy.py
@@ -1,6 +1,34 @@
 from pyoptimizer_analysis.ReadResultsStrategy import ReadResultsStrategy
-
+from pyoptimizer_analysis.Results import Results
+import pandas as pd
 
 class EDBOpResultsStrategy(ReadResultsStrategy):
-    def analyze_results(self, result_file: str):
-        raise RuntimeError("ReadResultsStrategy.analyze_results not overridden!")
+    def analyze_results(self, result_file: str)-> Results:
+        """Analyzes results from the EDBOP optimizer.
+
+        :param result_file: File to read the results from. This must be
+                            formatted as a JSON file.
+        :type result_file: str
+
+        :return: Aggregated results from the optimization.
+        :rtype: Results
+        """
+        
+        results = Results()
+        df = pd.read_csv(result_file)
+        data = df[df['priority']==-1]
+        data = data.drop('priority', axis=1)
+        data = data.reset_index(drop=True)
+        data['yield'] = pd.to_numeric(data['yield'])
+        print(data)
+
+        max_val = -data['yield'].max()
+        idmax = data['yield'].idxmax()+1
+
+        best_conditions = data.loc[data['yield'].idxmax()][:-1].values.tolist()
+
+        results.best_coords = best_conditions
+        results.best_value = max_val
+        results.best_iter = idmax
+        results.total_iter = 100
+        return results
