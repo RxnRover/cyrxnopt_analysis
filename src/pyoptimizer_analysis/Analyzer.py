44e3f5e350bdf33058bd97934032722a69a747c1
zachcran <zachcran@gmail.com>
2023-06-13 15:49:54 -0500

    Add 'analyze_directory' function

--- src/pyoptimizer_analysis/Analyzer.py
+++ src/pyoptimizer_analysis/Analyzer.py
@@ -1,17 +1,63 @@
+from typing import List
+
 from pyoptimizer_analysis.ReadResultsStrategy import ReadResultsStrategy
+from pyoptimizer_analysis.Results import Results
+from pyoptimizer_analysis.utilities.get_files import get_files
 
 
 class Analyzer:
     def __init__(self, read_strategy: ReadResultsStrategy):
         self._read_strategy = read_strategy
 
-    def analyze_results(self, results_file: str):
+    def analyze_results(self, results_file: str) -> Results:
+        """Analyzes the given file, accumulating the results for an
+        optimization algorithm.
+
+        The algorithm used to analyze the file is determined by the value
+        of read_strategy provided when the Analyzer was created.
+
+        :param results_file: Full path to the results file to analyze.
+        :type results_file: str
+        :return: Results object containing the results of the optimization.
+        :rtype: Results
+        """
+
         return self._read_strategy.analyze_results(results_file)
 
-    # def analyze_directory(self, results_directory: str):
+    def analyze_directory(
+        self,
+        results_directory: str,
+        file_pattern: str = r"*",
+        recursive: bool = False,
+    ) -> List[Results]:
+        """Analyzes all files in the given directory that match the provided
+        regex pattern.
 
-    #     # Read all the files os.walk
+        The results files must all be in the same format to be read correctly.
+        If results files are in a different format, incorrect results may be
+        collected or the program may crash.
+        The algorithm used to analyze the file is determined by the value
+        of read_strategy provided when the Analyzer was created.
 
-    #     for result_file in files:
+        :param results_directory: Full path to the results directory with
+                                  results files to analyze.
+        :type results_directory: str
+        :param file_pattern: Regex pattern that result files must match,
+                             defaults to r"*" for all files.
+        :type file_pattern: str
+        :param recursive: Whether to find files recursively in subdirectories
+                      (True) or not (False). Defaults to "False".
+        :type recursive: bool, optional
+        :return: Results object containing the results of the optimization.
+        :rtype: Results
+        """
 
-    #         result = self.analyze_results(result_file)
+        files = get_files(results_directory, file_pattern, recursive=recursive)
+
+        results = []
+        for result_file in files:
+            result = self.analyze_results(result_file)
+
+            results.append(result)
+
+        return results
