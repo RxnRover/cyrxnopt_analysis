d7d00a0242e0c581b6fb25a50dfd9b8a8565b62d
zachcran <zachcran@gmail.com>
2023-06-14 15:05:44 -0500

    Use 'os.path.join' to make paths platform-agnostic

--- tests/pyoptimizer_analysis/utilities/test_get_files.py
+++ tests/pyoptimizer_analysis/utilities/test_get_files.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 
 from pyoptimizer_analysis.utilities.get_files import get_files
@@ -5,10 +6,12 @@ from pyoptimizer_analysis.utilities.get_files import get_files
 
 class TestGetFiles(unittest.TestCase):
     def test_get_files_top_dir(self):
-        results_directory = "tests/example_results"
+        results_directory = os.path.join("tests", "example_results")
         results_pattern = r"[a-zA-z0-9]+_test.tmp"
 
-        corr_file_list = ["tests/example_results/config_test.tmp"]
+        corr_file_list = [
+            os.path.join("tests", "example_results", "config_test.tmp")
+        ]
 
         file_list = get_files(results_directory, results_pattern)
 
@@ -18,7 +21,9 @@ class TestGetFiles(unittest.TestCase):
         results_directory = "tests"
         results_pattern = r"[a-zA-z0-9]+_test.tmp"
 
-        corr_file_list = ["tests/example_results/config_test.tmp"]
+        corr_file_list = [
+            os.path.join("tests", "example_results", "config_test.tmp")
+        ]
 
         file_list = get_files(
             results_directory, results_pattern, recursive=True
