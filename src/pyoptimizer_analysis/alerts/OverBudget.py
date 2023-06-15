56fb2dd3ea881ec71612132d7b58161e85b0be09
zachcran <zachcran@gmail.com>
2023-06-15 16:35:47 -0500

    Test OverBudget alert

--- src/pyoptimizer_analysis/alerts/OverBudget.py
+++ src/pyoptimizer_analysis/alerts/OverBudget.py
@@ -9,15 +9,16 @@ class OverBudget(Alert):
         super(OverBudget, self).__init__(throw)
 
         self._budget = budget
+        self._errors = []
 
     def process(self, results: List[Results]):
         for result in results:
             # Skip the result if it is not tripping the alarm
             if result.total_iter <= self.budget:
-                break
+                continue
 
             # Store the result that is tripping the alarm
-            self.errors.append(result)
+            self._errors.append(result)
 
             # Throw an error
             if self.throw:
@@ -33,3 +34,7 @@ class OverBudget(Alert):
             raise ValueError("Cannot have a budget of <= 0.")
 
         self._budget = value
+
+    @property
+    def errors(self) -> List[Results]:
+        return self._errors
---
+++ tests/pyoptimizer_analysis/alerts/test_OverBudget.py
@@ -0,0 +1,53 @@
+import unittest
+
+from pyoptimizer_analysis.alerts.OverBudget import OverBudget
+from pyoptimizer_analysis.Results import Results
+
+
+class TestOverBudget(unittest.TestCase):
+    def test_no_throw_equals_budget_limit(self):
+        budget = 100
+        result = Results()
+        result.total_iter = budget
+
+        alert = OverBudget(budget, throw=True)
+
+        # This shouldn't throw
+        alert.process([result])
+
+    def test_no_throw_lessthan_budget_limit(self):
+        budget = 100
+
+        result = Results()
+        result.total_iter = budget - 1
+
+        alert = OverBudget(budget, throw=True)
+
+        # This shouldn't throw
+        alert.process([result])
+
+    def test_throw_greaterthan_budget_limit(self):
+        budget = 100
+
+        result = Results()
+        result.total_iter = budget + 1
+
+        alert = OverBudget(budget, throw=True)
+
+        self.assertRaises(RuntimeError, alert.process, [result])
+
+    def test_error_list(self):
+        budget = 100
+
+        result1 = Results()
+        result2 = Results()
+        result3 = Results()
+        result1.total_iter = budget
+        result2.total_iter = budget + 1
+        result3.total_iter = budget - 1
+
+        alert = OverBudget(budget, throw=False)
+
+        alert.process([result1, result2, result3])
+
+        self.assertListEqual(alert.errors, [result2])
