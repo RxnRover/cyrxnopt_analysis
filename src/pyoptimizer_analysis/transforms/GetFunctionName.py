d09c02db5a62005610fe043f31c9db8c9a7efead
zachcran <zachcran@gmail.com>
2023-06-16 12:19:55 -0500

    Add transform for adding function names

---
+++ src/pyoptimizer_analysis/transforms/GetFunctionName.py
@@ -0,0 +1,38 @@
+import os
+import re
+from typing import List
+
+from pyoptimizer_analysis.OptimizerResult import OptimizerResult
+from pyoptimizer_analysis.transforms.Transform import Transform
+
+
+class GetFunctionName(Transform):
+    def map(results: List[OptimizerResult]):
+        """Get the function name for each result based on the result file's
+        containing directory. The function name will be stored in the
+        result object under the "function" key.
+
+        :param results: Result list to transform.
+        :type results: List[OptimizerResult]
+        """
+
+        results = map(GetFunctionName._get_function_name, results)
+
+    def _get_function_name(result):
+        """Extracts the function name from the containing directory of
+        the results file and stores it in the Result object under the
+        "function" key.
+
+        :param result: Result object to get the function name for.
+        :type result: OptimizerResult
+        :return: Result object with the function name
+        :rtype: OptimizerResult
+        """
+
+        directory = os.path.basename(os.path.dirname(result.filename))
+        match = re.match(r"([a-zA-Z0-9]+)_[0-9]+", directory)
+        foo_name = match[1]
+
+        result["function"] = foo_name
+
+        return result
